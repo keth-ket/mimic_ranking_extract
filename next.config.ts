62a70ced56b920a0532cb8397a051991c53f0b5c
keth-ket <alice.ngnguyen@gmail.com>
2025-03-31 13:41:27 -0700

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Enables static export
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/your-repo-name" : "", // Only set basePath in production
+  assetPrefix: process.env.NODE_ENV === "production" ? "/your-repo-name/" : "", // Only set assetPrefix in production
 };
 
-export default nextConfig;
+module.exports = nextConfig;
